3ed900c30fdff138e66a703d3dc3226f8cdef0eb
Ajith G <Ajith.G@viswa.local>
2025-06-24 13:21:02 +0530

    remove 5000 port

--- app.py
+++ app.py
@@ -502,5 +502,5 @@ def filtered_average_particle_count():
 #         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
